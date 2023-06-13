f7fcd35fcdc7fde1b2c29d26f2786edbbfff882e
Costanza Rasi <costanza.rasi@uni-potsdam.de>
2023-06-14 01:30:03 +0200

    works now with updated einstellungen

--- berlin_bot.py
+++ berlin_bot.py
@@ -7,6 +7,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
 
 
 system = system()
@@ -59,10 +62,12 @@ class BerlinBot:
 
     @staticmethod
     def enter_form(driver: webdriver.Chrome):
+        wait = WebDriverWait(driver, 20)
+
         logging.info("Fill out form")
-        # select china
+        # select Bosnien und Herzegowina
         s = Select(driver.find_element(By.ID, 'xi-sel-400'))
-        s.select_by_visible_text("China")
+        s.select_by_visible_text("Bosnien und Herzegowina")
         # eine person
         s = Select(driver.find_element(By.ID, 'xi-sel-422'))
         s.select_by_visible_text("eine Person")
@@ -75,18 +80,30 @@ class BerlinBot:
         driver.find_element(By.XPATH, '//*[@id="xi-div-30"]/div[2]/label/p').click()
         time.sleep(2)
 
-        # click on study group
-        driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[1]/label/p').click()
+        # click on study group "studium und ausbildung"
+        # driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[1]/label/p').click()
+        # driver.find_element(By.XPATH, '//*[@id="xi-div-30"]/div[@class="ozg-kachel kachel-122-0-2 level1"]/label/p').click()
+        driver.find_element(By.XPATH, '//*[@id="inner-122-0-2"]/div/div[@class="ozg-accordion accordion-122-0-2-3 level2"]/label/p').click()
         time.sleep(2)
 
-        # b/c of stufy
-        driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[2]/div/div[5]/label').click()
+        # b/c of study "Aufenthaltserlaubnis für eine Berufsausbildung"
+        # driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[2]/div/div[5]/label').click()
+        driver.find_element(By.XPATH, '//*[@id="inner-122-0-2"]/div/div[@class="level2-content"][1]/div/div[@class="level3"][1]/label').click()
         time.sleep(4)
 
         # submit form
-        driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
+        try:
+            weiter_link = wait.until(EC.element_to_be_clickable((By.ID, 'applicationForm:managedForm:proceed')))
+            weiter_link.click()
+
+        except ElementClickInterceptedException:
+            logging.info("Trying to click on the Weiter button again")
+            driver.execute_script("arguments[0].click()", weiter_link)
+        # driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
+        # driver.find_element(By.XPATH,
+        #                     '//*[@id="applicationForm:managedForm:proceed"]/span[@class="ui-button-text ui-c"]').click()
         time.sleep(10)
-    
+
     def _success(self):
         logging.info("!!!SUCCESS - do not close the window!!!!")
         while True:
