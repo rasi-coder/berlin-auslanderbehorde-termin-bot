2988278d66b3790c4c3f822629c0a1f1c25e9749
Costanza Rasi <costanza.rasi@uni-potsdam.de>
2023-06-17 13:51:29 +0200

    updated for 18a

--- berlin_bot.py
+++ berlin_bot.py
@@ -64,13 +64,22 @@ class BerlinBot:
 
     @staticmethod
     def enter_form(driver: webdriver.Chrome):
-        wait = WebDriverWait(driver, 20)
+        try:
+            WebDriverWait(driver, 60).until(EC.invisibility_of_element((By.CLASS_NAME, 'loading')))
 
+        except:
+            return
         logging.info("Fill out form")
-        # select Bosnien und Herzegowina
-        s = Select(driver.find_element(By.ID, 'xi-sel-400'))
-        s.select_by_visible_text("Bosnien und Herzegowina")
 
+        # select Bosnien und Herzegowina
+        s = Select(WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.ID, 'xi-sel-400'))))
+        # s = Select(driver.find_element(By.ID, 'xi-sel-400'))
+        try:
+            s.select_by_visible_text("Bosnien und Herzegowina") #27
+        except:
+            return
+
+        time.sleep(1)
         # eine person
         s = Select(driver.find_element(By.ID, 'xi-sel-422'))
         s.select_by_visible_text("eine Person")
@@ -80,32 +89,47 @@ class BerlinBot:
         time.sleep(5)
 
         # extend stay
-        driver.find_element(By.XPATH, '//*[@id="xi-div-30"]/div[2]/label/p').click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="xi-div-30"]/div[2]/label/p'))).click()
+        # driver.find_element(By.XPATH, '//*[@id="xi-div-30"]/div[2]/label/p').click()
         time.sleep(2)
 
-        # click on study group "studium und ausbildung"
-        # driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[1]/label/p').click()
-        # driver.find_element(By.XPATH, '//*[@id="xi-div-30"]/div[@class="ozg-kachel kachel-122-0-2 level1"]/label/p').click()
-        driver.find_element(By.XPATH, '//*[@id="inner-122-0-2"]/div/div[@class="ozg-accordion accordion-122-0-2-3 level2"]/label/p').click()
+        # # click on study group "studium und ausbildung"
+        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="inner-122-0-2"]/div/div[@class="ozg-accordion accordion-122-0-2-3 level2"]/label/p'))).click()
+        # # driver.find_element(By.XPATH,
+        # #                     '//*[@id="inner-122-0-2"]/div/div[@class="ozg-accordion accordion-122-0-2-3 level2"]/label/p').click()
+        # time.sleep(2)
+
+        # OR click on Erwerbstaetigkeit
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                    '//*[@id="inner-122-0-2"]/div/div[@class="ozg-accordion accordion-122-0-2-1 level2"]/label'))).click()
         time.sleep(2)
 
-        # b/c of study "Aufenthaltserlaubnis für eine Berufsausbildung"
-        # driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[2]/div/div[5]/label').click()
-        driver.find_element(By.XPATH, '//*[@id="inner-122-0-2"]/div/div[@class="level2-content"][1]/div/div[@class="level3"][1]/label').click()
+        # # b/c of study "Aufenthaltserlaubnis für eine Berufsausbildung" 16a
+        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="inner-122-0-2"]/div/div[@class="level2-content"][1]/div/div[@class="level3"][1]/label'))).click()
+
+        # OR b/c of 18a
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                    '//*[@id="inner-122-0-2"]/div/div[@class="level2-content"][2]/div/div[@class="level3"][3]/label'))).click()
+
+        # driver.find_element(By.XPATH, '//*[@id="inner-122-0-2"]/div/div[@class="level2-content"][1]/div/div[@class="level3"][1]/label').click()
         time.sleep(4)
 
         # submit form
+        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'applicationForm:managedForm:proceed'))).click()
         try:
-            weiter_link = wait.until(EC.element_to_be_clickable((By.ID, 'applicationForm:managedForm:proceed')))
+            weiter_link = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'applicationForm:managedForm:proceed')))
             weiter_link.click()
 
         except ElementClickInterceptedException:
             logging.info("Trying to click on the Weiter button again")
             driver.execute_script("arguments[0].click()", weiter_link)
-        # driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-        # driver.find_element(By.XPATH,
-        #                     '//*[@id="applicationForm:managedForm:proceed"]/span[@class="ui-button-text ui-c"]').click()
+
         time.sleep(10)
+
+        try:
+            WebDriverWait(driver, 60).until(EC.invisibility_of_element((By.CLASS_NAME, 'loading')))
+        except:
+            return
 
     def _success(self):
         logging.info("!!!SUCCESS - do not close the window!!!!")
@@ -117,17 +141,54 @@ class BerlinBot:
 
     def run_once(self):
         with WebDriver() as driver:
-            self.enter_start_page(driver)
-            self.tick_off_some_bullshit(driver)
-            self.enter_form(driver)
+            try:
+                self.enter_start_page(driver)
+                self.tick_off_some_bullshit(driver)
 
-            # retry submit
-            for _ in range(10):
-                if not self._error_message in driver.page_source:
-                    self._success()
-                logging.info("Retry submitting form")
-                driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-                time.sleep(self.wait_time)
+                url_before = driver.current_url
+                self.enter_form(driver)
+
+                # retry submit
+                for _ in range(10):
+                    WebDriverWait(driver, 20).until(
+                        EC.element_to_be_clickable((By.ID, 'footer')))
+                    url_after = driver.current_url
+                    logging.info("Checking")
+                    if not self._error_message in driver.page_source \
+                            and not 'Familiäre Gründe' in driver.page_source \
+                            and url_before != url_after:
+                        current_window = driver.current_window_handle
+                        driver.execute_script("alert(\"Focus window\")")
+                        driver.switch_to.alert.accept()
+                        driver.switch_to.window(current_window)
+                        driver.fullscreen_window()
+                        self._success()
+                    time.sleep(10)
+                    logging.info("Retry submitting form")
+                    WebDriverWait(driver, 20).until(
+                        EC.element_to_be_clickable((By.ID,
+                                                    'applicationForm:managedForm:proceed'))).click()
+                    try:
+                        WebDriverWait(driver, 60).until(
+                            EC.invisibility_of_element(
+                                (By.CLASS_NAME, 'loading')))
+                    except:
+                        return
+            except Exception as e:
+                logging.error(e)
+                driver.switch_to.window(driver.current_window_handle)
+
+            # self.enter_start_page(driver)
+            # self.tick_off_some_bullshit(driver)
+            # self.enter_form(driver)
+            #
+            # # retry submit
+            # for _ in range(10):
+            #     if not self._error_message in driver.page_source:
+            #         self._success()
+            #     logging.info("Retry submitting form")
+            #     driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
+            #     time.sleep(self.wait_time)
 
     def run_loop(self):
         # play sound to check if it works
